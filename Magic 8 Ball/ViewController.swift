78c1b3ca63b5f907bec348e1c7bb534bd731a7fa
jimmy riguelle <jimmyriguelle@hotmail.com>
2018-10-14 15:19:19 +0200

    first step

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballarray: [String] = ["ball1","ball2","ball3","ball4","ball5"]
+    let randomBallNumber = 
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askQuestion(_ sender: Any) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
